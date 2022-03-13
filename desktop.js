8c40f4f5be208adf5b19710a488ce13a4ab4d760
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2022-03-13 13:15:17 +0530

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -1,3 +1,3 @@
-const bgs=["bg/bg1.jpg","bg/bg2.jpg","bg/bg3.jpg","bg/bg4.jpg","bg/bg5.jpg","bg/bg6.jpg"]
+const bgs=["bg/bg1.jpg","bg/bg2.jpg","bg/bg3.jpg","bg/bg4.jpg","bg/bg5.jpg","bg/bg6.jpg","bg/bg7.jpg","bg/bg8.jpg"]
 var item = bgs[Math.floor(Math.random()*bgs.length)];
 document.body.style.backgroundImage = "url("+item+")";
